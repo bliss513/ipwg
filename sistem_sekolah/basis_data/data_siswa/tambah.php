fa22245e11c15d4042b8a590ace2e84e9659d4f9
anjasninasaputri8 <anjasninasaputri790@gmail.com>
2024-08-13 15:01:59 +0700

    perubahan

--- sistem_sekolah/basis_data/data_siswa/tambah.php
+++ sistem_sekolah/basis_data/data_siswa/tambah.php
@@ -1,15 +1,16 @@
 <?php
-        include"../../config/koneksi.php";
+include "../../config/koneksi.php";
 
-        if(isset($_POST['simpan'])){
-            $logo= $_POST['nama'];
-            $logo= $_POST['nisn'];
+if(isset($_POST['simpan'])){
+    $nama = $_POST['nama'];
+    $nisn = $_POST['nisn'];
 
-            $sql = "INSERT INTO siswa(nama,nisn)VALUES('$nama','$nisn')";
-            if(mysqli_query($koneksi,$sql)){
-                header('location:index.php');
-            }else{
-                echo "Oupss....Maaf proses penyimpan data tidak berhasil";
-            }
-        }
-    ?>
+    $sql = "INSERT INTO siswa(nama, nisn) VALUES('$nama', '$nisn')";
+
+    if(mysqli_query($koneksi, $sql)){
+        header('location:../../button.php');
+    } else {
+        echo "Oupss....Maaf proses penyimpanan data tidak berhasil";
+    }
+}
+?>
