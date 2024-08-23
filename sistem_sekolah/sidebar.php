6bf926bd14d4d02fb74e0827374f63d3b3ab7b43
sifanurmazunifah <sifanurmazunifah@gmail.com>
2024-08-23 10:52:15 +0700

    p

--- sistem_sekolah/sidebar.php
+++ sistem_sekolah/sidebar.php
@@ -28,7 +28,12 @@
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-chart-bar me-2"></i>perpus</a>
                         <div class="dropdown-menu bg-transparent border-0">
-                        <a href="pinjaman.php" class="dropdown-item">pinjaman</a>
+                        <a href="tabel_pinjaman.php" class="dropdown-item">pinjaman</a>
+                   </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-table me-2"></i>absensi</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                        <a href="abs.php" class="dropdown-item">kelas</a>
                    </div>
                  </div>
                     <a href="form.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
