0f13b4338876003d0ac8f4c81b8f538659d44a94
bliss513 <blis.clth@gmail.com>
2024-08-29 14:50:30 +0700

    popop

--- sistem_sekolah/sidebar.php
+++ sistem_sekolah/sidebar.php
@@ -51,9 +51,9 @@
                             <a href="pembayaran.php" class="dropdown-item">pembayaran</a>
                         </div>
                      </div>
-                    <a href="form.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
+                    <!-- <a href="form.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a> -->
                     <!-- <a href="table.php" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a> -->
-                    <a href="chart.php" class="nav-iclasstem nav-link"><i class="fa fa-chart-bar me-2"></i>chart</a>
+                    <!-- <a href="chart.php" class="nav-iclasstem nav-link"><i class="fa fa-chart-bar me-2"></i>chart</a> -->
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
                         <div class="dropdown-menu bg-transparent border-0">
